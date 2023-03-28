265919afe2373c52bf07009a13e50e456da2d4c7
tegarraflyy <127176780+tegarraflyy@users.noreply.github.com>
2023-03-28 08:14:47 +0700

    Add files via upload

---
+++ modul 3 jS.js
@@ -0,0 +1,18 @@
+
+
+var firstName = "tegar";
+var lastName = "Fajar";
+var age = 19;
+var isMarried = false;
+
+
+console.log("First Name: " + firstName);
+console.log("Last Name: " + lastName);
+console.log("Age: " + age);
+console.log("Is Married: " + isMarried);
+
+
+document.write("<p>First Name: " + firstName + "</p>");
+document.write("<p>Last Name: " + lastName + "</p>");
+document.write("<p>Age: " + age + "</p>");
+document.write("<p>Is Married: " + isMarried + "</p>");
