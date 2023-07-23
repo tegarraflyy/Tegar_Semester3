dbaa9d6018f10eacaf166e4a5097995b2fa21dd7
Tegar Rafly AlFajar <127176780+tegarraflyy@users.noreply.github.com>
2023-07-23 23:33:01 +0700

    Add files via upload

---
+++ modul 02/variabel.js
@@ -0,0 +1,14 @@
+
+// variabel var
+var nama = "tegar rafly alfajar", kelas = "RPL B";
+console.log(nama);
+console.log(kelas);
+
+// variabel let
+let firstname;
+ firstname = "Tegar";
+ console.log(firstname);
+
+ // variabel const
+ const a = 19
+ console.log(umur = (a))
